400e797c22eb2121a38186159bb2bf9d63c4adb5
ZeuCode <86081746+ZeuCode@users.noreply.github.com>
2025-11-09 23:37:23 -0500

    Se quito LINK del helpage

--- src/pages/help/HelpPage.tsx
+++ src/pages/help/HelpPage.tsx
@@ -330,7 +330,7 @@ import {
   BarChart,
   Lightbulb,
 } from "lucide-react";
-import { Link, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import { useState } from "react";
 
 export default function HelpPage() {
